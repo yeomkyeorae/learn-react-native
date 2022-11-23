77ee9f936be11621c9424e47276bb3ba23f7ea11
yeomkyeorae <duarufp06@naver.com>
2022-11-23 23:32:03 +0900

    -

--- components/Greeting.js
+++ components/Greeting.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-function Greeting() {
+function Greeting(props) {
   return (
-    <View>
-      <Text>Greeting Component</Text>
-    </View>
+    <>
+      <View>
+        <Text>Greeting Component {props.name}</Text>
+      </View>
+      <Text>Extra!</Text>
+    </>
   );
 }
+
+Greeting.defaultProps = {
+  name: '리액트 네이티브',
+};
 
 export default Greeting;
